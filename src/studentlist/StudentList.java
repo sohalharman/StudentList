ec0aa515e5438b804e56295b3331e23733c36151
sohalharman <sohaharm@sheridancollege.ca>
2023-06-08 08:55:54 -0400

    This is my 4th commit in which i revert the 3rd commit to the 2nd commit on June 8, 2023 at 8:55 AM.

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -11,7 +11,6 @@ public class StudentList {
      */
     public static void main(String[] args) {
         // This is a comment I added on 8:36 AM
-        // This is the second comment I added on 8:38 AM
     }
 
 }
