c1238a467dd13a15c78735cb231e952ab348571b
sohalharman <sohaharm@sheridancollege.ca>
2023-06-08 08:18:12 -0400

    This is my first commit for StudentList project on June 8, 2023 at 8:07 AM.

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,16 @@
+package studentlist;
+
+/**
+ * This class +++ Insert class description here +++
+ * @author Harmandeep Singh Sohal
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+    }
+
+}
